2b85fec5341b635faf4cdf768a4656a06e0152e9
Xuyang Tang <tangxuyang.hi@163.com>
2015-01-07 20:57:14 +0800

    add TextAnalyzer.cs

--- Downloader/Program.cs
+++ Downloader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Downloader
 {
@@ -10,7 +11,19 @@ namespace Downloader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            FileStream fs = new FileStream("..\\..\\text.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string str = sr.ReadToEnd();
+            var strs = TextAnalyzer.Analyzer(str);
+            
+            strs.ForEach(o => Console.WriteLine(o));
+            Console.WriteLine(strs.Count);
+            Console.ReadLine();
         }
+    }
+
+    class Person
+    {
+        public int? Name { get; set; }
     }
 }
---
+++ Downloader/TextAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Downloader
+{
+    public class TextAnalyzer
+    {
+        private static string aPattern = "<a\\s{1,}([A-Za-z0-9#:\\.\\s=\"'/\\-_&\\?\\%\u4e00-\u9fa5]*)>";
+        public static string hrefPattern = "href\\s*=\\s*\"[A-Za-z0-9\\.\\s=\\-_&\\?\\%/:]*\"";
+        public static List<string> Analyzer(string text)
+        {
+            
+            Regex regex = new Regex(aPattern);
+            Regex regex2 = new Regex(hrefPattern);
+            var matches = regex.Matches(text);
+            List<string> result = new List<string>();
+            foreach (Match match in matches)
+            {
+                //result.Add(match.Value);
+                var ms = regex2.Matches(match.Value);
+                foreach(Match m in ms)
+                {
+                    result.Add(m.Value);
+                }
+            }
+
+            return result;
+        }
+    }
+}
